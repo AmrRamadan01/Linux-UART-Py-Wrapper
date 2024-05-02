791373f21b4452ebc071d71c9bbfe9f6c96ffa0a
Amr Ramadan <78983720+AmrRamadan01@users.noreply.github.com>
2024-05-02 22:59:20 +0300

    feat: create py_serial_test.py

---
+++ src/py_serial_test.py
@@ -0,0 +1,40 @@
+import time
+import serial
+
+# Configure the serial connection (parameters may differ based on your device)
+ser = serial.Serial(
+    port='/dev/ttyS0',  # Specify the serial port (e.g., '/dev/ttyUSB1')
+    baudrate=15200,        # Set the baud rate
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+)
+
+ser.isOpen()
+print("Enter your commands below.")
+print("Insert \"exit\" to leave the application.")
+
+while True:
+    # Get keyboard input
+    # user_input = input(">> ")
+
+    # if user_input == 'exit':
+    #     ser.close()
+    #     break
+    # else:
+        # Send the character to the device
+        # (Note: I've added a carriage return and line feed to the characters as requested by my device)
+    
+    user_input = "Aloo"
+    ser.write(user_input + '\r\n')
+
+    # Wait for the device to respond
+    time.sleep(1)
+
+    # Read the output
+    out = ''
+    while ser.inWaiting() > 0:
+        out += ser.read(1)
+
+    if out:
+        print(">> " + out)
